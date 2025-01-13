d4f28b58d517c9b0c6be04225424d2bdb8b3bda3
routechan <mr0326h14@gmail.com>
2025-01-13 16:45:54 +0900

    contents部分作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,44 @@ import './App.css'
 import Header from "./components/Header"
 import {motion} from 'motion/react'
 import { Variants } from "framer-motion";
-
+import { Github, Mail,ExternalLink, Linkedin, Code2, Globe, Database, Server, Cpu, Cloud, Lock } from 'lucide-react';
+import { useState } from 'react';
 function App() {
+
+  // モーダルウィンドウの開閉
+const [isOpen,setIsOpen] = useState(false)
+const openModal = ():void =>{
+  setIsOpen(true);
+}
+const closeModal = ():void =>{
+  setIsOpen(false)
+}
+
+  // my favorite photos
  const gridImages:string[] = ["public/images/grid/grid-1.jpg","public/images/grid/grid-2.jpg","public/images/grid/grid-3.jpg","public/images/grid/grid-4.jpg","public/images/grid/grid-5.jpg","public/images/grid/grid-6.jpg","public/images/grid/grid-7.jpg","public/images/grid/grid-8.jpg"]
 
+//  skills
+ interface Skill{
+  skillName:string,
+  terms:string[]
+}
+const skills:Skill[] = [{skillName:"Frontend",terms:["HTML","CSS","React"]},{skillName:"Backend",terms:["PHP","Laravel","Firebase"]},{skillName:"DevOps",terms:["Docker", "Kubernetes", "AWS"]}]
+
+// works
+interface Work{
+  workName:string,
+  description:string,
+  image:string,
+  link:string
+}
+const works:Work[] = [
+  {workName:"AIバーテンダー",description:"あなたの手元にある材料、アルコールの強さ、味、今の気分を入力するとAIがオリジナルのカクテルを考え、レシピなどを教えてくれます",image:"mori-801135634_0212_D_TP_V4.jpg",link:"https://ai-bartender-67d9c.web.app/"},
+  {workName:"AIバーテンダー",description:"あなたの手元にある材料、アルコールの強さ、味、今の気分を入力するとAIがオリジナルのカクテルを考え、レシピなどを教えてくれます",image:"mori-801135634_0212_D_TP_V4.jpg",link:"https://ai-bartender-67d9c.web.app/"},
+  {workName:"AIバーテンダー",description:"あなたの手元にある材料、アルコールの強さ、味、今の気分を入力するとAIがオリジナルのカクテルを考え、レシピなどを教えてくれます",image:"mori-801135634_0212_D_TP_V4.jpg",link:"https://ai-bartender-67d9c.web.app/"}
+]
+
+
+// framer motion用のvariants
 const variants:Variants = {
   offscreen:{ 
     y:70,
@@ -25,7 +59,7 @@ const variants:Variants = {
     <div>
 
 
-<section className="relative bg-[url('/images/ai2445CITY008-image_TP_V.jpg')] bg-cover min-h-screen font-HindMadurai">
+<section id="Home" className="relative bg-[url('/images/ai2445CITY008-image_TP_V.jpg')] bg-cover no-repeat center center bg-fixed min-h-screen font-HindMadurai">
   {/* 色を重ねるためのafter擬似要素 */}
   <div className="absolute inset-0 bg-gradient-to-r from-sky-50 to-indigo-50 opacity-70"></div>
 <Header/>
@@ -35,7 +69,7 @@ animate={{scale:1}}
 transition={{type:"spring",duration:0.7,delay:0.4}}
 className='relative z-20 min-h-screen flex flex-col justify-center items-center'>
 
-<h1 className='text-7xl font-bold text-center '>Routechan Portfolio</h1>
+<h1 className='text-5xl md:text-6xl font-bold text-center '>Routechan Portfolio</h1>
 <div className='text-center mt-6 '>
   <motion.img
   initial={{scale:0.9}}
@@ -43,17 +77,17 @@ className='relative z-20 min-h-screen flex flex-col justify-center items-center'
   src='../public/images/mori-801135634_0212_D_TP_V4.jpg' alt='プロフィール画像' className='w-64 h-64 rounded-full object-cover'/>
 </div>
 <p className='text-xl text-center font-bold mt-2'>Engineer</p>
-<div className='flex gap-4 mt-4 text-2xl'>
+<div className='flex flex-col md:flex-row gap-4 mt-4 text-2xl'>
   <motion.a
   whileHover={{ scale: 1.1 }}
-  href='#' className='px-6 py-3 bg-gray-800 text-white rounded-full flex items-center gap-2 hover:bg-gray-700'>Github</motion.a>
+  href='#' className='px-6 py-3 bg-gray-800 text-white rounded-full flex items-center gap-2 hover:bg-gray-700'><Github/>Github</motion.a>
   <motion.a      
   whileHover={{ scale: 1.1 }}
   href="#contact"
   className="px-6 py-3 bg-sky-500 text-white rounded-full flex items-center gap-2 hover:bg-sky-400"
               >
              
-                Contact
+                <Mail/>Contact
               </motion.a>
 </div>
 
@@ -61,21 +95,21 @@ className='relative z-20 min-h-screen flex flex-col justify-center items-center'
      </section>
 
      {/* プロフィール */}
-     <section id='Profile' className='bg-gradient-to-r from-sky-50 to-indigo-50 '>
-      <div className='max-w-4xl mx-auto py-32'>
+     <section id='Profile' className='bg-sky-50'>
+      <div className='max-w-4xl mx-auto py-32 px-8 md:px-4 '>
      <motion.h2
      variants={variants}
      initial="offscreen"
      whileInView="onscreen"
      viewport={{once:true}}
-     className='text-center text-6xl'>Profile</motion.h2>
+     className='text-center text-3xl font-bold md:text-6xl'>Profile</motion.h2>
 
      <motion.div
       variants={variants}
       initial="offscreen"
       whileInView="onscreen"
       viewport={{once:true}}
-     className='flex gap-6 mt-8 justify-between'>
+     className=' md:flex gap-6 mt-8 justify-between'>
       <div className='flex-1 '>
         <img src='../public/images/mori-801135634_0212_D_TP_V4.jpg' alt='プロフィール画像' className='rounded-lg shadow-md'/>
       </div>
@@ -90,7 +124,7 @@ className='relative z-20 min-h-screen flex flex-col justify-center items-center'
       initial="offscreen"
       whileInView="onscreen"
       viewport={{once:true}}
-     className='text-3xl text-center'>My Favorite Photos</motion.h3>
+     className='text-2xl md:text-3xl text-center'>My Favorite Photos</motion.h3>
 
      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {gridImages.map((src, index) => (
@@ -99,7 +133,7 @@ className='relative z-20 min-h-screen flex flex-col justify-center items-center'
           whileInView={{opacity:1,y:0}}
           transition={{duration:0.7,delay:0.5 + index * 0.2}}
           viewport={{once:true}}
-          key={index} className="relative overflow-hidden rounded-lg shadow-md">
+          key={index} className="relative overflow-hidden shadow-md">
             <img
               src={src}
               alt={`Gallery ${index + 1}`}
@@ -112,8 +146,158 @@ className='relative z-20 min-h-screen flex flex-col justify-center items-center'
      </div>
      </div>
 
-     <motion.div drag whileDrag={{scale:1.2}} className='w-16 h-16 bg-orange-500 rounded-md'></motion.div>
+   
      </section>
+
+
+{/* Skill */}
+     <section id="Skill" className='bg-slate-50'>
+     
+
+     <div className='relative z-20 max-w-5xl mx-auto py-32 px-8 md:px-4'>
+     <motion.h2
+     variants={variants}
+     initial="offscreen"
+     whileInView="onscreen"
+     viewport={{once:true}}
+     className='text-center text-3xl font-bold md:text-6xl'>Skill</motion.h2>
+   
+   
+
+
+     {/* skillカード */}
+<div className='mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+
+{skills.map((skill,index)=>(
+  <motion.div
+  initial={{opacity:0,y:50}}
+          whileInView={{opacity:1,y:0}}
+          transition={{duration:0.7,delay:0.5 + index * 0.2}}
+          viewport={{once:true}}
+  key={index} 
+  className='min-h-48 bg-white shadow-lg p-4'>
+<h3 className='text-center text-2xl font-bold'>{skill.skillName}</h3>
+<div className='mt-4 flex gap-2'>
+  {skill.terms.map((term,idx)=>(
+    <span key={idx} className='bg-gray-100 p-2 rounded-3xl'>{term}</span>
+  ))}
+</div>
+</motion.div>
+))}
+
+</div>
+
+</div>
+
+     </section>
+
+     {/* Work */}
+     <section id="Work" className='bg-sky-50'>
+     <div className='relative z-20 max-w-5xl mx-auto py-32 px-8 md:px-4'>
+     <motion.h2
+     variants={variants}
+     initial="offscreen"
+     whileInView="onscreen"
+     viewport={{once:true}}
+     className='text-center text-3xl font-bold md:text-6xl'>Work</motion.h2>
+
+
+      <div className="mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+        {works.map((work,index)=>(
+          <motion.div
+          initial={{opacity:0,y:50}}
+          whileHover={{ scale: 1.05 ,duration:0.3}}
+          whileInView={{opacity:1,y:0}}
+          transition={{
+            opacity: { duration: 0.7, delay: 0.5 + index * 0.2 }, // whileInView用
+            y: { duration: 0.7, delay: 0.5 + index * 0.2 },       // whileInView用
+            scale: { duration: 0.3 },                             // whileHover用
+          }}
+          viewport={{once:true}}
+          key={index} 
+          className='border-gray-50 rounded-md bg-white overflow-hidden cursor-pointer shadow-lg'>
+  <div className='h-48 overflow-hidden'>
+    <img src={`../public/images/${work.image}`}/>
+  </div>
+
+ <div className='p-4'>
+  <h3>{work.workName}</h3>
+  <p className='mt-4'>{work.description}</p>
+  <div className='flex justify-end mt-4'>
+  <a href={work.link} target="_blank" className='' >
+                      <ExternalLink size={20} />
+                    </a>
+  </div>
+ </div>
+</motion.div>
+        ))}
+
+</div>
+</div>
+
+     </section>
+
+
+{/* Contact */}
+<section id="Contact" className='bg-slate-100'>
+
+      <div className='max-w-4xl mx-auto py-32 px-8 md:px-4'>
+     <motion.h2
+     variants={variants}
+     initial="offscreen"
+     whileInView="onscreen"
+     viewport={{once:true}}
+     className='text-center text-3xl font-bold md:text-6xl'>Contact</motion.h2>
+
+<motion.div
+variants={variants}
+initial="offscreen"
+whileInView="onscreen"
+viewport={{once:true}}
+whileHover={{ scale: 1.05 }}
+className='text-center mt-16'>
+  <button onClick={openModal} className=' bg-sky-500 rounded-lg text-white text-xl md:text-2xl p-4 '>
+  お問い合わせはこちらをクリック
+  </button>
+</motion.div>
+
+<div
+onClick={closeModal}
+ className={`${
+  isOpen ? "bg-black bg-opacity-70 z-20 w-full h-full fixed inset-0 flex flex-col justify-center items-center" : "hidden"
+}`}>
+<div
+onClick={(e) => e.stopPropagation()}
+className='bg-slate-50 border-gray-50 border rounded-lg shadow-lg p-8 relative z-30'>
+  <h3 className='text-center'>お問い合わせ</h3>
+  <form>
+    <ul>
+      <li>
+    <label htmlFor='name'>名前</label>
+    <input id='name' type='text'></input>
+    </li>
+    <li>
+    <label htmlFor='mail'>メールアドレス</label>
+    <input id='mail' type='mail'></input>
+    </li>
+    <li>
+    <label htmlFor='content'>お問い合わせ内容</label>
+    <textarea id="content"></textarea>
+    </li>
+<li>
+    <input type='submit' value="送信"/>
+    </li>
+    </ul>
+  </form>
+
+</div>
+</div>
+
+     </div>
+</section>
+
+
 
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'motion/react'
+import { Menu, X } from 'lucide-react';
 
 const Header = () => {
+  // ナビゲーションメニュー
     const navLists:string[] = ["Home","Profile","Work","Contact"] //ナビゲーションリストの配列
 
+    // はんばーがーめにゅー
+    const [isMenuOpen,setIsMenuOpen] = useState(false)
+const toggleMenu = ():void =>{
+  setIsMenuOpen(!isMenuOpen);
+}
   return (
     <motion.header
     
     initial={{y:-100}}
     animate={{y:0}}
     transition={{type:"spring",duration:0.7,delay:0.7,damping: 9}}
-    className='bg-transparent fixed top-0 w-full z-50'>
-        <div className='flex justify-between items-center container mx-auto h-16'>
+    className='bg-white md:bg-transparent fixed top-0 w-full z-50 shadow-sm md:shadow-none'>
+        <div className='flex justify-between items-center container px-8 mx-auto h-16'>
             <div className='text-2xl'>Logo</div>
 
-            <nav>
+
+            <nav className='hidden md:block'>
             <ul className="flex gap-8 text-2xl">
             {navLists.map((navlist) => (
               <motion.li
@@ -30,12 +38,44 @@ const Header = () => {
   whileHover={{ width: "100%" }}
   transition={{ duration: 0.3 }}
 />
-
               </motion.li>
             ))}
           </ul>
             </nav>
+
+            {/* SP用ハンバーガーメニュー */}
+<div className="md:hidden">
+            <button
+            onClick={toggleMenu}
+              className="text-gray-600 hover:text-gray-900"
+            >
+              {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+            </button>
+          </div>
+          
         </div>
+
+        {isMenuOpen && (
+  <motion.div
+    initial={{ height: 0, opacity: 0 }}
+    animate={{ height: "auto", opacity: 1 }}
+    exit={{ height: 0, opacity: 0 }}
+    transition={{ duration: 0.2 }}
+    className="bg-white py-4 px-8 text-gray-600 shadow-sm md:hidden  overflow-hidden container mx-auto"
+  >
+    <nav>
+      <ul className="flex flex-col gap-4 text-xl">
+        {navLists.map((navList, index) => (
+          <li key={index}> {/* navList自体をキーに使用 */}
+            <a href={`#${navList}`} className="hover:text-blue-500">
+              {navList}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </motion.div>
+)}
     </motion.header>
   )
 }
